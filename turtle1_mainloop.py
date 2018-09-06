c04cfa85b6f0af88c90ff475238f03516095743b
whentea <wentyyuniarti@gmail.com>
2018-09-06 22:49:07 +0700

    module, type, methods, mainloop

---
+++ turtle1_mainloop.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1  let's play turtle
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     06/09/2018
+# Copyright:   (c) ASUS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import turtle               # load a module named turtle
+wn = turtle.Screen()        # a type in turtle. create anda open window
+myarrow = turtle.Turtle()   # make an object myarrow
+myarrow.forward(50)         # methods forward
+myarrow.left(90)            # methods left
+myarrow.forward(30)         # methods forward
+turtle.mainloop()           # cause we use python 2.7
