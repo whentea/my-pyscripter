69921b986909f9c44586c781580dfaa0bdf83b57
whentea <wentyyuniarti@gmail.com>
2018-09-06 23:17:50 +0700

    modif myarrow

---
+++ turtle2_myarrow_modif.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     06/09/2018
+# Copyright:   (c) ASUS 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import turtle
+wn =turtle.Screen()
+wn.bgcolor("blue")
+wn.title("Color Array")
+mynewarrow = turtle.Turtle()
+mynewarrow.color("red")
+mynewarrow.pensize(3)
+mynewarrow.forward(50)
+mynewarrow.left(120)
+mynewarrow.forward(50)
+turtle.mainloop()
